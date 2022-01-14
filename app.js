dd3787f4db8904ce6a19e917bdcd1c16a90e8c55
Marce Serrano <smarcelo75@hotmail.com>
2022-01-14 19:33:11 -0300

    Generación del archivo Tabla-5.txt

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const tabla = 5;
+const nombreArchivo = `Tabla-${tabla}.txt`;
+let datos = '';
+
+for (let i = 1; i < 11; i++) {
+    datos += `${tabla} x ${i} = ${tabla*i} \n`;
+}
+
+fs.writeFile(nombreArchivo, datos, (err) => {
+    if (err)
+        console.log('Error al generar el archivo')
+    else
+        console.log(`Se genero el archivo [${nombreArchivo}] correctamente!!`);
+})
